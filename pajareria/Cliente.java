e8473d0fb4478929c9f8de6ec219ad50263914c4
Cristian250800 <cristianmancha15@gmail.com>
2025-07-30 17:11:30 +0200

    Creación e implantación de la funcionalidad "Alta de clientes"

--- pajareria/Cliente.java
+++ pajareria/Cliente.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Cliente {
 
         private String nombre;
@@ -6,7 +8,27 @@ public class Cliente {
         private String email;
 
 
+        public Cliente(String nombre, String dni, String telefono, String email) {
+                this.nombre = nombre;
+                this.dni = dni;
+                this.telefono = telefono;
+                this.email = email;
+        }
 
+        public static Cliente altaNuevoCliente() {
 
+                Scanner altaCliente = new Scanner(System.in);
 
+                System.out.println("Introduce tu nombre completo:");
+                String nombre = altaCliente.nextLine();
+                System.out.println("Introduce tu dni con letra incluida: ");
+                String dni = altaCliente.nextLine();
+                System.out.println("Introduce tu número de teléfono: ");
+                String telefono = altaCliente.nextLine();
+                System.out.println("Introduce tu email: ");
+                String email = altaCliente.nextLine();
+
+                return new Cliente(nombre,dni, telefono,email);
+
+        }
 }
--- pajareria/main.java
+++ pajareria/main.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
 
+    static ArrayList <Cliente> clientes = new ArrayList<>();
 
+    public main() {
+    }
 
     public static void main(String[] args) {
 
@@ -10,18 +14,44 @@ public class main {
     int opcion;
     do {
         System.out.println("BIENVENIDO A PICO Y PLUMA");
-        System.out.println("Introduce una opción para continuar: ");
         System.out.println("1. Gestión de clientes ");
         System.out.println("2. Gestión de pájaros ");
         System.out.println("3. Realizar venta ");
         System.out.println("4. Mostrar ventas ");
         System.out.println("5. Salir");
+        System.out.println("Introduce una opción para continuar: ");
 
         opcion = scanner.nextInt();
         scanner.nextLine();
 
         switch (opcion){
-            case 1 ->{}
+            case 1 ->{
+
+                System.out.println("Gestión de clientes");
+                System.out.println("Escoge una opción para continuar: ");
+                System.out.println("1. Alta de clientes");
+                System.out.println("2. Baja de clientes");
+                System.out.println("3. Modificación de clinetes");
+                System.out.println("4. Búsqueda por DNI");
+                System.out.println("5. Listado de clientes");
+
+                opcion = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (opcion){
+                    case 1 ->{
+                        Cliente.altaNuevoCliente();
+                        System.out.println("¡Cliente dado de alta satisfactoriamente!");
+                    }
+                    case 2 ->{}
+                    case 3 ->{}
+                    case 4 ->{}
+                    case 5 ->{}
+                    default ->{
+                        System.out.println("Introduce una opción válida");}
+
+                }
+            }
             case 2 ->{}
             case 3 ->{}
             case 4 ->{}
@@ -33,7 +63,7 @@ public class main {
 
         }
 
-    }while (opcion !=0 );
+    }while (opcion !=5 );
 
     }
 }
