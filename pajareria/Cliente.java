210cc32db27884dcda92b74140ad7511b105d564
Cristian250800 <cristianmancha15@gmail.com>
2025-07-31 16:29:22 +0200

    Creación e implantación de la funcionalidad "Modificación de clientes y búsqueda por DNI"

--- pajareria/Cliente.java
+++ pajareria/Cliente.java
@@ -27,6 +27,7 @@ public class Cliente {
                 String telefono = altaCliente.nextLine();
                 System.out.println("Introduce tu email: ");
                 String email = altaCliente.nextLine();
+                System.out.println("¡Cliente dado de alta satisfactoriamente!");
 
                 return new Cliente(nombre,dni, telefono,email);
 
@@ -45,6 +46,23 @@ public class Cliente {
                 return email;
         }
 
+        public void guardarNombre(String nombre) {
+                this.nombre = nombre;
+        }
+
+        public void guardarDNI(String dni) {
+                this.dni = dni;
+        }
+
+        public void guardarTelefono(String telefono) {
+                this.telefono = telefono;
+        }
+
+        public void guardarEmail(String email) {
+                this.email = email;
+        }
+
+
         public static void bajaCliente(){
                 Scanner scanner = new Scanner(System.in);
 
@@ -56,7 +74,7 @@ public class Cliente {
                 for (Cliente c: main.clientes){
                         if (c.buscarNombre().toLowerCase().equals(eliminarCliente)) {
                                 encontrado = true;
-                                System.out.println("Cliente encontrado"+
+                                System.out.println("¡Cliente encontrado!"+
                                         "\nNombre: " + c.buscarNombre() +
                                         "\nDNI: " + c.buscarDNI() +
                                         "\nTeléfono: " + c.buscarTelefono() +
@@ -69,7 +87,7 @@ public class Cliente {
 
                                 if (confirmar) {
                                         main.clientes.remove(c);
-                                        System.out.println("Cliente eliminado con éxito");
+                                        System.out.println("¡Cliente dado de baja satisfactoriamente!");
                                 } else {
                                         System.out.println("Se ha cancelado el proceso de eliminación");
 
@@ -85,4 +103,111 @@ public class Cliente {
 
 
         }
+
+
+        public static void modificarCliente(){
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Introduce el nombre del cliente que deseas modificar: ");
+
+                String clienteModificar = scanner.nextLine().toLowerCase();
+
+                boolean encontrado = false;
+                for (Cliente c: main.clientes){
+                        if (c.buscarNombre().toLowerCase().equals(clienteModificar)) {
+                                encontrado = true;
+                                System.out.println("¡Cliente encontrado!"+
+                                        "\nNombre: " + c.buscarNombre() +
+                                        "\nDNI: " + c.buscarDNI() +
+                                        "\nTeléfono: " + c.buscarTelefono() +
+                                        "\nE-mail: " + c.buscarEmail());
+
+                                System.out.println("¿Deseas modificar este cliente? (true/false):");
+                                boolean confirmar = scanner.nextBoolean();
+                                scanner.nextLine();
+
+
+                                if (confirmar) {
+                                        System.out.println("¿Qué registro deseas modificar? Indica la opción.");
+                                        System.out.println("1. Nombre");
+                                        System.out.println("2. DNI");
+                                        System.out.println("3. Teléfono");
+                                        System.out.println("4. e-mail");
+                                        System.out.println("5. Salir del proceso");
+
+                                        int registro = scanner.nextInt();
+                                        scanner.nextLine();
+
+                                        switch (registro){
+                                                case 1 -> {
+                                                        System.out.println("Introduce el nuevo nombre:");
+                                                        String nuevoNombre = scanner.nextLine();
+                                                        c.guardarNombre(nuevoNombre);
+                                                        System.out.println("Nombre actualizado correctamente.");
+                                                }
+                                                case 2 -> {
+                                                        System.out.println("Introduce el nuevo DNI:");
+                                                        String nuevoDni = scanner.nextLine();
+                                                        c.guardarDNI(nuevoDni);
+                                                        System.out.println("DNI actualizado correctamente.");
+                                                }
+                                                case 3 -> {
+                                                        System.out.println("Introduce el nuevo número de teléfono:");
+                                                        String nuevoTelefono = scanner.nextLine();
+                                                        c.guardarTelefono(nuevoTelefono);
+                                                        System.out.println("Teléfono actualizado correctamente.");
+                                                }
+                                                case 4 -> {
+                                                        System.out.println("Introduce el nuevo email:");
+                                                        String nuevoEmail = scanner.nextLine();
+                                                        c.guardarEmail(nuevoEmail);
+                                                        System.out.println("Email actualizado correctamente.");
+                                                }
+
+                                                case 5 -> {
+                                                        System.out.println("Has salido del proceso de modificación");
+                                                }
+                                        }
+                                } else {
+                                        System.out.println("Se ha cancelado el proceso de modificación");
+
+                                }
+                                break;
+                        }
+
+                }
+                if (!encontrado){
+                        System.out.println("No se encontró ningún cliente con ese nombre");
+                }
+
+
+
+        }
+
+
+        public static void BuscarPorDNI() {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Introduce el DNI de la persona que quieres buscar: ");
+                String DNIaBuscar = scanner.nextLine();
+
+                boolean encontrado = false;
+
+                for (Cliente c : main.clientes) {
+                        if (c.buscarDNI().equals(DNIaBuscar)) {
+                                System.out.println("¡Cliente encontrado!" +
+                                        "\nNombre: " + c.buscarNombre() +
+                                        "\nDNI: " + c.buscarDNI() +
+                                        "\nTeléfono: " + c.buscarTelefono() +
+                                        "\nE-mail: " + c.buscarEmail());
+                                encontrado = true;
+                                break;
+                        }
+                }
+
+                if (!encontrado) {
+                        System.out.println("Cliente no encontrado");
+                }
+        }
+
 }
--- pajareria/main.java
+++ pajareria/main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -18,7 +20,8 @@ public class main {
         Scanner scanner = new Scanner(System.in);
     int opcion;
     do {
-        System.out.println("BIENVENIDO A PICO Y PLUMA");
+        System.out.println("-------------------------------------");
+        System.out.println("     BIENVENIDO A PICO Y PLUMA");
         System.out.println("1. Gestión de clientes ");
         System.out.println("2. Gestión de pájaros ");
         System.out.println("3. Realizar venta ");
@@ -36,7 +39,7 @@ public class main {
                 System.out.println("Escoge una opción para continuar: ");
                 System.out.println("1. Alta de clientes");
                 System.out.println("2. Baja de clientes");
-                System.out.println("3. Modificación de clinetes");
+                System.out.println("3. Modificación de clientes");
                 System.out.println("4. Búsqueda por DNI");
                 System.out.println("5. Listado de clientes");
 
@@ -46,14 +49,16 @@ public class main {
                 switch (opcion){
                     case 1 ->{
                         Cliente.altaNuevoCliente();
-                        System.out.println("¡Cliente dado de alta satisfactoriamente!");
                     }
                     case 2 ->{
                         Cliente.bajaCliente();
-
                     }
-                    case 3 ->{}
-                    case 4 ->{}
+                    case 3 ->{
+                        Cliente.modificarCliente();
+                    }
+                    case 4 ->{
+                        Cliente.BuscarPorDNI();
+                    }
                     case 5 ->{}
                     default ->{
                         System.out.println("Introduce una opción válida");}
